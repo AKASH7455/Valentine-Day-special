4054c5ecccb6009d36c20d39dd975235db0852a6
akashkumar7455 <ubayveer5@gmail.com>
2026-01-09 19:32:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
 
+
 function moveNoButton() {
   const btnRect = noBtn.getBoundingClientRect();
 
@@ -27,8 +28,54 @@ noBtn.addEventListener("touchstart", moveNoButton);
 
 // Yes button click
 yesBtn.addEventListener("click", () => {
-  document.querySelector(".question").textContent =
-    "I knew it ❤️";
-  document.querySelector(".subtitle").textContent =
-    "You just made my day!";
+  const question = document.querySelector(".question");
+  const subtitle = document.querySelector(".subtitle");
+
+  // text change
+  question.textContent = "I LOVE YOU ❤️";
+  subtitle.textContent = "Take a screenshot and send it to your partner.";
+
+  // animation reset (important trick)
+  question.classList.remove("text-animate");
+  subtitle.classList.remove("text-animate");
+
+  void question.offsetWidth; // force reflow
+  void subtitle.offsetWidth;
+
+  // animation add
+  question.classList.add("text-animate");
+  subtitle.classList.add("text-animate");
 });
+
+document.getElementById("yesBtn").addEventListener("click", fireConfetti);
+
+function fireConfetti() {
+  const defaults = {
+    spread: 360,
+    ticks: 100,
+    gravity: 0,
+    decay: 0.94,
+    startVelocity: 30,
+    shapes: ["heart"],
+    colors: ["#FFC0CB", "#FF69B4", "#FF1493", "#C71585"],
+  };
+
+  confetti({
+    ...defaults,
+    particleCount: 50,
+    scalar: 2,
+  });
+
+  confetti({
+    ...defaults,
+    particleCount: 25,
+    scalar: 3,
+  });
+
+  confetti({
+    ...defaults,
+    particleCount: 10,
+    scalar: 4,
+  });
+}
+ 
